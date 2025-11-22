112afcd1d0082309d76b83da1a96104d9f8969db
vagabundoo <evillamorm@gmail.com>
2025-11-22 14:59:45 +0100

    Working on 2021, solved days 1 and 2

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -3,13 +3,32 @@
 public class Day01 : BaseDay
 {
     private readonly string _input;
-
+    private int _solution1;
+    private int _solution2;
     public Day01()
     {
         _input = File.ReadAllText(InputFilePath);
+        var lines = _input.Split(Environment.NewLine);
+        
+        var count = 0;
+        for (var i = 0; i < lines.Length - 1; i++)
+            if (int.Parse(lines[i]) < int.Parse(lines[i + 1]))
+                count++;
+        _solution1 = count;
+
+        count = 0;
+        for (var i = 0; i < lines.Length - 3; i++)
+        {
+            var currentWindowSum = int.Parse(lines[i]) + int.Parse(lines[i + 1]) + int.Parse(lines[i + 2]);
+            var nextWindowSum = int.Parse(lines[i + 1]) + int.Parse(lines[i + 2]) + int.Parse(lines[i + 3]);
+            if (currentWindowSum < nextWindowSum)
+                count++;
+        }
+
+        _solution2 = count; 
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{_solution1}");
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() => new($"{_solution2}");
 }
---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode;
+
+public class Day02 : BaseDay
+{
+    private readonly string _input;
+    private int _solution1;
+    private int _solution2;
+    public Day02()
+    {
+        
+        _input = File.ReadAllText(InputFilePath);
+        var lines = _input.Split(Environment.NewLine);
+        var horizontalPosition = 0;
+        var depth = 0;
+        
+        foreach (var instruction in lines)
+        {
+            var direction = instruction.Split(' ')[0];
+
+            var distance = int.Parse(instruction.Split(' ')[1]);
+            
+            switch (direction)
+            {
+                case "forward":
+                    horizontalPosition += distance;
+                    break;
+                case "down":
+                    depth += distance;
+                    break;
+                case "up":
+                    depth -= distance;
+                    break;
+            }
+            
+            _solution1 = horizontalPosition * depth;
+        }
+        
+        //Part 2
+        var aim = 0;
+        horizontalPosition = 0;
+        depth = 0;
+        
+        foreach (var instruction in lines)
+        {
+            var direction = instruction.Split(' ')[0];
+
+            var distance = int.Parse(instruction.Split(' ')[1]);
+            
+            switch (direction)
+            {
+                case "forward":
+                    horizontalPosition += distance;
+                    depth += (aim * distance);
+                    break;
+                case "down":
+                    aim += distance;
+                    break;
+                case "up":
+                    aim -= distance;
+                    break;
+            }
+            
+            _solution2 = horizontalPosition * depth;
+        }
+        
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{_solution1}");
+
+    public override ValueTask<string> Solve_2() => new($"{_solution2}");
+}
