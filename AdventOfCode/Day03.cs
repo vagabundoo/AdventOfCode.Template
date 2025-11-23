7bca3adb00b36d2d0544bf367eea1fb7f16540cf
vagabundoo <evillamorm@gmail.com>
2025-11-23 12:08:15 +0100

    Solved day 4 part 1.

---
+++ AdventOfCode/Day03.cs
@@ -0,0 +1,80 @@
+﻿namespace AdventOfCode;
+
+public class Day03 : BaseDay
+{
+    private readonly string _input;
+    private int _solution1;
+    private int _solution2;
+    
+    public Day03()
+    {
+        _input = File.ReadAllText(InputFilePath);
+        var lines = _input.Split(Environment.NewLine);
+        
+        var gammaRate = "";
+        var epsilonRate = "";
+        for (var i = 0; i < lines[0].Length; i++)
+        {
+            var oneBits = lines.Count(line => line[i] == '1');
+            var zeroBits = lines.Count(line => line[i] == '0');
+            
+            if (oneBits > zeroBits)
+            {
+                gammaRate += "1";
+                epsilonRate += "0";
+            }
+            else
+            {
+                gammaRate += "0";
+                epsilonRate += "1";
+            }
+        }
+        
+        _solution1 = Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
+        
+        var oxygenGeneratorRating = "";
+        var co2ScrubberRating = "";
+        
+        var filteredLines = lines;
+        for (var i = 0; i < lines[0].Length; i++)
+        {
+            var oneBits = filteredLines.Count(line => line[i] == '1');
+            var zeroBits = filteredLines.Count(line => line[i] == '0');
+
+            if (oneBits >= zeroBits)
+                filteredLines = filteredLines.Where(line => line[i] == '1').ToArray();
+            else
+                filteredLines = filteredLines.Where(line => line[i] == '0').ToArray();
+
+            if (filteredLines.Length == 1)
+            {
+                oxygenGeneratorRating = filteredLines[0];
+                break;
+            }
+        }
+
+        filteredLines = lines;
+        for (var i = 0; i < lines[0].Length; i++)
+        {
+            var oneBits = filteredLines.Count(line => line[i] == '1');
+            var zeroBits = filteredLines.Count(line => line[i] == '0');
+        
+            if (zeroBits <= oneBits)
+                filteredLines = filteredLines.Where(line => line[i] == '0').ToArray();
+            else
+                filteredLines = filteredLines.Where(line => line[i] == '1').ToArray();
+
+            if (filteredLines.Length == 1)
+            {
+                co2ScrubberRating = filteredLines[0];
+                break;
+            }
+        }
+        
+        _solution2 = Convert.ToInt32(oxygenGeneratorRating, 2) * Convert.ToInt32(co2ScrubberRating, 2);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{_solution1}");
+
+    public override ValueTask<string> Solve_2() => new($"{_solution2}");
+}
---
+++ AdventOfCode/Day04.cs
@@ -0,0 +1,83 @@
+﻿namespace AdventOfCode;
+
+public class Day04 : BaseDay
+{
+    private readonly string _input;
+    private int _solution1;
+    private int _solution2;
+    
+    public Day04()
+    {
+        _input = File.ReadAllText(InputFilePath);
+        
+        var lines = _input.Split(Environment.NewLine);
+        
+        var bingoNumbers = lines[0].Split(',').Select(int.Parse).ToList();
+        
+        var boardsInput = lines[2..]
+            .Select(line => line
+                .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList()).ToList();
+        
+        // To make up for missing empty last row
+        boardsInput.Add(new List<int>());
+        
+        var boards = new List<Board>();
+        for (var i = 0; i < boardsInput.Count - 5; i += 6)
+        {
+            var rows = boardsInput.Skip(i).Take(5).ToList();
+            var columns = new List<List<int>>();
+            for (var j = 0; j < 5; j++)
+            {
+                var column = rows.Select(row => row[j]).ToList();
+                columns.Add(column);
+                
+            }
+
+            boards.Add(new Board(rows, columns));
+
+        }
+        
+
+        foreach (var number in bingoNumbers)
+        {
+            foreach (var board in boards)
+            {
+                board.markNumber(number);
+                if (board.IsBingo())
+                {
+                    var unmarkedNumbers = board.AllNumbers.Except(board.MarkedNumbers).ToList();
+                    _solution1 = unmarkedNumbers.Sum() * number;
+                    return;
+                }
+            }
+        }
+
+
+        
+    }
+
+    public class Board(List<List<int>> rows, List<List<int>> columns)
+    {
+        public List<List<int>> Rows { get; } = rows;
+        public List<List<int>> Columns { get; } = columns;
+
+        public IEnumerable<int> AllNumbers => Rows.SelectMany(row => row);
+        public IEnumerable<int> MarkedNumbers = [];
+
+        public void markNumber(int number)
+        {
+            if (AllNumbers.Contains(number)) {
+                MarkedNumbers = MarkedNumbers.Concat([number]);
+            }
+        }
+        
+        public bool IsBingo() => 
+            Rows.Any(row => row.All(number => MarkedNumbers.Contains(number))) || 
+            Columns.Any(column => column.All(number => MarkedNumbers.Contains(number)));
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{_solution1}");
+
+    public override ValueTask<string> Solve_2() => new($"{_solution2}");
+}
